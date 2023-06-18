aa6faba88a835e64c270c0b1b1b4f4c7c731c633
OctaDem <octadem@gmail.com>
2023-06-18 07:09:04 -0700

    qwe

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -9,23 +9,19 @@
  * Return: Always 0.
 */
 
-int mani(void)
+int main(void)
 {
 	int n;
 
-	srand(time(0))
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
-	{
-	 	printf("%d is positive\n" , n);
-	}
-	else if (n == 0)
-	{
-	       	printf("%d is zero\n", n);
-	}
-	else
-	{
+		printf("%d is positive\n", n);
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	}	
+	else
+		printf("%d is zero\n", n);
+
 	return (0);
 }	
