c3aec1e6bdfded2af2c8c7129bc5f5c0fdd25609
OctaDem <octadem@gmail.com>
2023-06-18 06:56:04 -0700

    qwe

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,19 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Prints a random number and states whether
+ *        it is positive, negative, or zero.
+ *
+ * Return: Always 0.
+*/
+
 int mani(void)
 {
 	int n;
 
 	srand(time(0))
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 	 	printf("%d is positive\n" , n);
