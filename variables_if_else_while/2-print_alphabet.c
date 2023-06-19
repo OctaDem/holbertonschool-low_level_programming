576e6dbb9b21a9d34c1b1a58fa8d52521c3ead63
OctaDem <octadem@gmail.com>
2023-06-19 06:11:44 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+
+	{
+		putchar(abc);
+
+		abc++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
