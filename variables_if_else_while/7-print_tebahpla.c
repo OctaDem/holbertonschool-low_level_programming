1016cae20d79edc3b08e6986a08ffc1f588aa34e
OctaDem <octadem@gmail.com>
2023-06-19 09:54:38 -0700

    zwx

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char abc = 'z';
+
+	while (abc <= 'a')
+
+{
+
+	putchar(abc);
+
+	abc++;
+}
+
+	putchar('\n');
+
+	return (0);
+}
