833506f1c85cc2600fae931877b3994de912d34f
OctaDem <octadem@gmail.com>
2023-06-20 11:10:42 -0700

    qwe

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char = abc;
+
+	for (abc = 'a'; abc <= 'z'; abc++)
+
+	{
+	
+		_putchar (abc);
+	
+	}
+
+	_putchar ('\n');
+
+}
